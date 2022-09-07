7bb90c021cc3670a58218ed7ee2151c264e74369
Tran Binh Co <tranbinhco99@gmail.com>
2022-09-07 16:19:23 +0700

    Config app and server

---
+++ src/modules/index/index.controller.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+export default class IndexController{
+    // tách ra để overide lại ở từng phương thức
+    public index = (req: Request, res:Response, next: NextFunction)=>{
+        try {
+            res.status(200).send('API is running...')
+        } catch (error) {
+            //có lỗi xảy ra thì next tới middleware tiếp theo xử lý
+            next(error);
+        }
+    };
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,5 @@
-import express,{Request,Response} from 'express' 
-
-//khai báo port
-const port = process.env.PORT || 5000;
-
-//tạo server express
-
-const app = express();
-
-app.get('/', (req: Request, res:Response)=>{
-    res.send('API is running...')
-});
-
-app.listen(port,()=>{
-    console.log(`Server is listening on port ${port}`);
-});
+import App from "./app";
+import { IndexRoute } from "./modules/index";
+const routes = [new IndexRoute()]
+const app = new App(routes);
+app.listen();
