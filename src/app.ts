a13da4c1c0cbc43b4c78ade39c0aa0e9ae497784
Tran Binh Co <tranbinhco99@gmail.com>
2022-09-08 10:47:39 +0700

    Connect database mongodb with mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Route } from './core/interfaces';
 import express from 'express';
+import mongoose from 'mongoose';
 class App{
     public app : express.Application;
     public port : string | number;
@@ -8,6 +9,7 @@ class App{
         this.app = express();
         this.port = process.env.PORT ||  5000;
         this.initializeRoutes(routes);
+        this.connectToDatabase();
     }
 
     public listen(){
@@ -19,6 +21,18 @@ class App{
         routes.forEach((route)=>{
             this.app.use("/",route.router)
         })
+    }
+
+    private connectToDatabase(){
+        try {
+            const connectString = "mongodb+srv://tranbinhco:0915741856@master.897bagq.mongodb.net/preschool_social?retryWrites=true&w=majority";
+            mongoose.connect(connectString,{
+            })
+            console.log('Database connected...')
+        } catch (error) {
+            console.log('Connect to database error')
+            
+        }
     }
 }
 export default App;
